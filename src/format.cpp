0794bc5b3ef3adaca2cb397c1d8d2c16204be283
joseppii <i.piperakis@gmail.com>
2020-02-22 23:48:26 +0100

    Implement elapsedTime

--- src/format.cpp
+++ src/format.cpp
@@ -1,11 +1,18 @@
 #include <string>
-
+#include <iomanip>
 #include "format.h"
 
 using std::string;
 
-// TODO: Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) { 
+
+    std::ostringstream str;
+    int hours = seconds / 3600;
+    int minutes = (seconds % 3600)/60;
+    int secs = seconds % 60;
+    
+    str <<std::dec << std::setw(2) << std::setfill('0') << hours << ":" << minutes << ":" << secs;
+    return str.str(); 
+}
--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -2,7 +2,7 @@
 #include <unistd.h>
 #include <string>
 #include <vector>
-
+#include <iostream>
 #include "linux_parser.h"
 
 using std::stof;
@@ -70,7 +70,19 @@ vector<int> LinuxParser::Pids() {
 float LinuxParser::MemoryUtilization() { return 0.0; }
 
 // TODO: Read and return the system uptime
-long LinuxParser::UpTime() { return 0; }
+long LinuxParser::UpTime() { 
+  std::string line;
+  long uptime;
+  
+  std::ifstream stream(kProcDirectory + kUptimeFilename);
+  if (stream.is_open()) {
+    std::getline(stream, line);
+    std::istringstream linestream(line);
+    linestream >> uptime ;
+  } 
+
+  return uptime; 
+}
 
 // TODO: Read and return the number of jiffies for the system
 long LinuxParser::Jiffies() { return 0; }
