be794509fc5120c5c1f4e931259fe6d2ad2de754
joseppii <i.piperakis@gmail.com>
2020-02-27 01:05:27 +0100

    Accomodate for uptime in days

--- src/format.cpp
+++ src/format.cpp
@@ -9,10 +9,15 @@ using std::string;
 string Format::ElapsedTime(long seconds) { 
 
     std::ostringstream str;
-    int hours = seconds / 3600;
+    int days = seconds / 86400;
+    int hours = (seconds % 86400) / 3600;
     int minutes = (seconds % 3600)/60;
     int secs = seconds % 60;
     
-    str <<std::dec << std::setw(2) << std::setfill('0') << hours << ":" << minutes << ":" << secs;
+    str <<std::dec << std::setfill('0') 
+                   << std::setw(2) << days << ":" 
+                   << std::setw(2) << hours << ":"
+                   << std::setw(2) << minutes << ":" 
+                   << std::setw(2) << secs;
     return str.str(); 
 }
